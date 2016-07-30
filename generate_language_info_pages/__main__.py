a53ff805cb4a96383d80bf11cfea9cfad89e1e2c
Sander Maijers <S.N.Maijers@gmail.com>
2016-07-30 15:27:02 +0200

    Clean up
    * Use standard import rules as required by pre-commit.
    * Apply YAPF 0.11.0 style.
    * Shorten names and lines.

--- generate_language_info_pages/__main__.py
+++ generate_language_info_pages/__main__.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
+from concurrent.futures import as_completed
+from concurrent.futures import ThreadPoolExecutor
+from json import loads
+from logging import error
+from logging import getLogger
+from logging import info
+from logging import WARNING
+from logging import warning
 from pathlib import Path
+from sys import stdout
+from tempfile import mkdtemp
+
+from mako.template import Template
+from requests import get
+from requests import Session
+from requests.adapters import HTTPAdapter
+from requests.status_codes import codes
 
 
-def process_language(resource, endangered_languages_http_connection_pool, output_dir_path: Path, template):
-    from logging import info
-    from requests.status_codes import codes
-
-    identifiers = {identifier['type']: identifier['identifier'] for identifier in resource['identifiers']}
+def process_language(resource, langs_http_conn_pool, output_dir_path: Path,
+                     template):
+    identifiers = {identifier['type']: identifier['identifier']
+                   for identifier in resource['identifiers']}
     if 'iso639-3' in identifiers:
         identifiers['iso639'] = identifiers.pop('iso639-3')
         identifiers['glottolog'] = resource['id']
@@ -15,72 +29,67 @@ def process_language(resource, endangered_languages_http_connection_pool, output
         if 'WALS' not in identifiers:
             identifiers['WALS'] = ''
 
-        endangered_languages_http_request_response = endangered_languages_http_connection_pool.head(
+        langs_http_rqst_resp = langs_http_conn_pool.head(
             'http://www.endangeredlanguages.com/lang/' + identifiers['iso639'])
 
-        language_info_page_file_path = output_dir_path.joinpath(identifiers['iso639'] + '.html')
+        language_info_page_file_path = output_dir_path.joinpath(identifiers[
+            'iso639'] + '.html')
 
-        with language_info_page_file_path.open(mode='wt', encoding='utf-8') as language_info_page_file:
-            info('Written language info page {language_info_page_file_path}'.format(
-                language_info_page_file_path=language_info_page_file_path))
+        with language_info_page_file_path.open(
+                mode='wt', encoding='utf-8') as language_info_page_file:
+            info(
+                'Written language info page {'
+                'language_info_page_file_path}'.format(
+                    language_info_page_file_path=language_info_page_file_path))
             language_info_page_file.write(
-                template.render(iso639=identifiers['iso639'],
-                                glottolog=identifiers['glottolog'],
-                                name=identifiers['name'],
-                                wals=identifiers['WALS'],
-                                is_on_endangered_languages=endangered_languages_http_request_response.status_code ==
-                                                           codes['OK']))
+                template.render(
+                    iso639=identifiers['iso639'],
+                    glottolog=identifiers['glottolog'],
+                    name=identifiers['name'],
+                    wals=identifiers['WALS'],
+                    is_on_endangered_languages=langs_http_rqst_resp.status_code
+                    == codes['OK']))
 
 
 def main():
-    from concurrent.futures import ThreadPoolExecutor, as_completed
-    from json import loads
-    from logging import error, warning
-    from mako.template import Template
-    from pathlib import Path
-    from requests import get, Session
-    from requests.adapters import HTTPAdapter
-    from sys import stdout
-    from tempfile import mkdtemp
-
-    glottlog_response = get('http://glottolog.org/resourcemap.json?rsc=language')
+    glottlog_response = get(
+        'http://glottolog.org/resourcemap.json?rsc=language')
     glottolog_mapping = loads(glottlog_response.text)
 
-    template = Template(filename='data/language_info.template.html',
-                        input_encoding='utf-8')
+    template = Template(
+        filename='data/language_info.template.html', input_encoding='utf-8')
 
     output_dir_path = Path(mkdtemp(prefix='langinfo_'))
 
-    warning("Creating language info pages in '{output_dir_path}'.".format(output_dir_path=output_dir_path))
+    warning("Creating language info pages in '{output_dir_path}'.".format(
+        output_dir_path=output_dir_path))
 
-    with Session() as HTTP_connection_pool:
+    with Session() as http_conn_pool:
         adapter = HTTPAdapter(pool_connections=25, pool_maxsize=25)
-        HTTP_connection_pool.mount('http://www.endangeredlanguages.com/lang/', adapter)
+        http_conn_pool.mount('http://www.endangeredlanguages.com/lang/',
+                             adapter)
         number_of_languages = len(glottolog_mapping['resources'])
 
         with ThreadPoolExecutor(max_workers=4) as executor:
-            future_to_response = dict((executor.submit(process_language,
-                                                       language,
-                                                       HTTP_connection_pool,
-                                                       output_dir_path,
-                                                       template),
-                                       language)
-                                      for language in glottolog_mapping['resources'])
+            future_to_response = dict(
+                (executor.submit(process_language, language, http_conn_pool,
+                                 output_dir_path, template), language)
+                for language in glottolog_mapping['resources'])
             resource_index = 0
             for future in as_completed(future_to_response):
                 resource_index += 1
                 if future.exception() is not None:
-                    error('Thread generated an exception: {0}'.format(future.exception()))
+                    error('Thread generated an exception: {0}'.format(
+                        future.exception()))
                 else:
                     progress = resource_index / number_of_languages
                     block = int(round(80 * progress))
-                    text = "\rProgress: [{0:s}] {1:0.3f} %".format("#" * block + "-" * (80 - block), progress * 100)
+                    text = "\rProgress: [{0:s}] {1:0.3f} %".format(
+                        "#" * block + "-" * (80 - block), progress * 100)
                     stdout.write(text)
                     stdout.flush()
 
 
 if __name__ == '__main__':
-    from logging import getLogger, WARNING
-
     getLogger().setLevel(WARNING)
     main()
