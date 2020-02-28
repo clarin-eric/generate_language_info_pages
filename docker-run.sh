3f188cfc648fab88b8c6080f6fd64c3a3dece767
Twan Goosen <twan@clarin.eu>
2020-02-28 14:50:37 +0100

    make sure that an absolute path is used for host mount (info pages target location) to container

--- docker-run.sh
+++ docker-run.sh
@@ -11,14 +11,14 @@ OUTPUT_DIR="$1"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 mkdir -p "${OUTPUT_DIR}"
-TMP_DIR="${OUTPUT_DIR}/.tmp-$(date +%Y%m%d%H%M%S)"
+OUTPUT_DIR_ABSOLUTE="$( cd "${OUTPUT_DIR}" >/dev/null && pwd )"
+TMP_DIR="${OUTPUT_DIR_ABSOLUTE}/temp-$(date +%Y%m%d%H%M%S)"
 
 if [ -e "${TMP_DIR}"/* ]; then
 	echo "Noise found in temporary directory ${TMP_DIR}, please remove manually. Aborted"
 	exit 1
 fi
 
-mkdir -p "${OUTPUT_DIR}"
 
 # Build
 (cd "${SCRIPT_DIR}" && docker build --tag "generate_language_info_pages:latest" .)
@@ -29,7 +29,7 @@ docker run --rm -v "${TMP_DIR}":"/tmp" "generate_language_info_pages:latest"
 
 # Check output
 if [ -d "${TMP_DIR}"/* ]; then
-  mv "${TMP_DIR}"/* "${OUTPUT_DIR}"
+  mv "${TMP_DIR}"/* "${OUTPUT_DIR_ABSOLUTE}"
   echo -e "\nResult available in ${OUTPUT_DIR}"
   rmdir "${TMP_DIR}"
 else
